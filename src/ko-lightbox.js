58b2e4175f57a2d00126a61d80947b09dd8d96b2
Matthew Nitschke <mnitschke@faithchapel.cc>
2016-06-10 13:52:08 -0600

    Namespaced styles

--- src/ko-lightbox.js
+++ src/ko-lightbox.js
@@ -1,38 +1,38 @@
 ko.components.register("lightbox", {
-  viewModel: function(params){
-    var self = this;
+    viewModel: function (params) {
+        var self = this;
 
-    var defaults = {
-      closeButton: true,
-      background: "#FFF"
-    }
+        var defaults = {
+            closeButton: true,
+            background: "#FFF"
+        }
 
-    var options = extend(defaults, params);
+        var options = extend(defaults, params);
 
-    self.viewModel = params.viewModel;
-    self.viewModel.lightbox = self;
+        self.viewModel = params.viewModel;
+        self.viewModel.lightbox = self;
 
-    self.visible = params.visible;
+        self.visible = params.visible;
 
-    self.closeButton = options.closeButton;
-    self.background = options.background;
+        self.closeButton = options.closeButton;
+        self.background = options.background;
 
-    self.close = function(){
-      self.visible(false);
-    }
+        self.close = function () {
+            self.visible(false);
+        }
 
-    function extend(a, b){
-      for(var key in b)
-          if(b.hasOwnProperty(key))
-              a[key] = b[key];
-      return a;
-    }
+        function extend(a, b) {
+            for (var key in b)
+                if (b.hasOwnProperty(key))
+                    a[key] = b[key];
+            return a;
+        }
 
-  },
-  template: "<div class='lightbox' data-bind='visible: visible'>\
-              <div class='lightbox-content-wrapper' data-bind='with: viewModel'>\
-                <div class='lightbox-close-button' data-bind='if: $parent.closeButton'><i class='fa fa-times' aria-hidden='true' data-bind='click: close'></i></div>\
-                <div class='lightbox-content' data-bind='attr: {style: \"background-color: \" + $parent.background} '>\
+    },
+    template: "<div class='ko__lightbox' data-bind='visible: visible'>\
+              <div class='ko__lightbox-content-wrapper' data-bind='with: viewModel'>\
+                <div class='ko__lightbox-close-button' data-bind='if: $parent.closeButton'><i class='fa fa-times' aria-hidden='true' data-bind='click: close'></i></div>\
+                <div class='ko__lightbox-content' data-bind='attr: {style: \"background-color: \" + $parent.background} '>\
                   <!-- ko template: { nodes: $componentTemplateNodes } --><!-- /ko -->\
                 </div>\
               </div>\
